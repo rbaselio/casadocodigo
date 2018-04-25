a364136f6a94bf21f1ec97102134e0a2277d77c6
roberto <roberto@roberto-Lenovo-ideapad-320-15IKB>
2018-04-25 19:48:43 -0300

    erro redirecionamento

---
+++ src/main/java/br/com/casadocodigo/loja/models/CarrinhoCompras.java
@@ -0,0 +1,20 @@
+package br.com.casadocodigo.loja.models;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CarrinhoCompras {
+
+    private Set<CarrinhoItem> itens = new HashSet<>();
+
+    public void add(CarrinhoItem item) {
+        itens.add(item);
+    }
+
+	public List<CarrinhoItem> getItens() {
+		return new ArrayList<CarrinhoItem>(itens);
+	}
+
+}
