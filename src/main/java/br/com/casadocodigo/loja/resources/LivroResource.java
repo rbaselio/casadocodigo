4339c827efd49c4afbc6d115d208633fdac5b161
roberto <roberto@roberto-Lenovo-ideapad-320-15IKB>
2018-05-11 21:09:52 -0300

    Disponibilizando os lançamentos em XML e JSON

---
+++ src/main/java/br/com/casadocodigo/loja/resources/LivroResource.java
@@ -0,0 +1,35 @@
+package br.com.casadocodigo.loja.resources;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.jboss.resteasy.annotations.providers.jaxb.Wrapped;
+
+import br.com.casadocodigo.loja.daos.LivroDao;
+import br.com.casadocodigo.loja.models.Livro;
+
+@Path("livros")
+public class LivroResource {
+	
+	@Inject
+	private LivroDao dao;
+
+	@GET
+	@Path("lancamentos")
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	@Wrapped(element="livros")
+    public List<Livro> ultimosLancamentosJson() {
+		System.out.println("OK");
+        return dao.ultimosLancamentos();
+    }
+	
+	
+
+
+
+}
